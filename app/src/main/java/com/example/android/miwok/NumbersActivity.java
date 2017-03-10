009745afc130ef77388ecf1c9539277630740890
Lucas Tormin <lucasntormin@gmail.com>
2017-03-09 21:28:15 -0300

    Added the MediaPlayer feature to play the words when the user clicks on the list item.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,8 +15,11 @@
  */
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.android.miwok.adapters.WordAdapter;
@@ -27,30 +30,76 @@ import java.util.List;
 
 public class NumbersActivity extends AppCompatActivity {
 
+    private MediaPlayer mediaPlayer;
+
+    private MediaPlayer.OnCompletionListener completionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        List<Word> words = new ArrayList<>();
+        final List<Word> words = new ArrayList<>();
 
-        words.add(new Word("one", "lutti",R.drawable.number_one));
-        words.add(new Word("two", "otiiko",R.drawable.number_two));
-        words.add(new Word("three", "tolookusu",R.drawable.number_three));
-        words.add(new Word("four", "oyyisa",R.drawable.number_four));
-        words.add(new Word("five", "massokka",R.drawable.number_five));
-        words.add(new Word("six", "temmokka", R.drawable.number_six));
-        words.add(new Word("seven", "kennekaku", R.drawable.number_seven));
-        words.add(new Word("eight", "kawinta",R.drawable.number_eight));
-        words.add(new Word("nine", "wo'e",R.drawable.number_nine));
-        words.add(new Word("ten", "na'aacha",R.drawable.number_ten));
+        words.add(new Word("one", "lutti",R.drawable.number_one, R.raw.number_one));
+        words.add(new Word("two", "otiiko",R.drawable.number_two, R.raw.number_two));
+        words.add(new Word("three", "tolookusu",R.drawable.number_three,R.raw.number_three));
+        words.add(new Word("four", "oyyisa",R.drawable.number_four, R.raw.number_four));
+        words.add(new Word("five", "massokka",R.drawable.number_five, R.raw.number_five));
+        words.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
+        words.add(new Word("seven", "kennekaku", R.drawable.number_seven, R.raw.number_seven));
+        words.add(new Word("eight", "kawinta",R.drawable.number_eight, R.raw.number_eight));
+        words.add(new Word("nine", "wo'e",R.drawable.number_nine, R.raw.number_nine));
+        words.add(new Word("ten", "na'aacha",R.drawable.number_ten, R.raw.number_ten));
 
         WordAdapter itemsAdapter = new WordAdapter(this, words,R.color.category_numbers);
 
         ListView listView = (ListView) findViewById(R.id.list);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                releaseMediaPlayer();
+
+                Word word = words.get(i);
+                mediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioResourceId());
+                mediaPlayer.setOnCompletionListener(completionListener);
+                mediaPlayer.start();
+            }
+        });
+
         listView.setAdapter(itemsAdapter);
 
 
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+        }
     }
 }
